423a0a41a56c3b28bd64e01728cb123d04c55670
Fred Vuni <fredvuni809@gmail.com>
2023-09-01 13:31:33 +0300

    add image to articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,17 +3,23 @@ import React from "react";
 const Articles = () => {
   return (
     <div class="article-container">
-      <div class="article">
-        <img src="soccer.jpg" alt="Soccer Image" class="article-image" />
-        <div class="article-content">
-          <h2 class="article-title">Exciting Soccer Match</h2>
-          <p class="article-details">
-            In a thrilling match, the home team secured victory with a
-            last-minute goal.
-          </p>
-          <p class="article-date">Published: 2023-08-25</p>
+      <a href="">
+        <div class="article">
+          <img
+            src="https://images.ctfassets.net/hrltx12pl8hq/725XwMfBbE8pTlx5k2232Q/3780839851ff54ca71d5d9d0e45e9a58/03-beauty-fashion_1781009285.jpg?fit=fill&w=840&h=473&fm=webp"
+            alt="Soccer Image"
+            class="article-image"
+          />
+          <div class="article-content">
+            <h2 class="article-title">Exciting Soccer Match</h2>
+            <p class="article-details">
+              In a thrilling match, the home team secured victory with a
+              last-minute goal.
+            </p>
+            <p class="article-date">Published: 2023-08-25</p>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
