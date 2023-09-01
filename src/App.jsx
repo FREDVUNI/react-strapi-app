92ce3fbafff05bd3e5317105e6e07473311f6a77
Fred Vuni <fredvuni809@gmail.com>
2023-09-01 12:43:14 +0300

    initial set up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Articles from "./components/Articles"
+
+function App() {
+
+  return (
+    <div>
+      <Articles/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const articles = [
+  {
+    id: 1,
+    title: "Exciting Soccer Match",
+    details:
+      "In a thrilling match, the home team secured victory with a last-minute goal.",
+    image: "soccer.jpg",
+    createdAt: "2023-08-25T14:30:00Z",
+  },
+  {
+    id: 2,
+    title: "New Science Discoveries",
+    details:
+      "Scientists have made groundbreaking discoveries in the field of astrophysics.",
+    image: "astrophysics.jpg",
+    createdAt: "2023-08-24T10:15:00Z",
+  },
+  {
+    id: 3,
+    title: "Tech Giant's Product Launch",
+    details:
+      "A leading tech company unveiled its latest gadgets, leaving tech enthusiasts excited.",
+    image: "tech.jpg",
+    createdAt: "2023-08-23T16:45:00Z",
+  },
+  {
+    id: 4,
+    title: "Travelers' Paradise",
+    details:
+      "Explore the hidden gems of a beautiful tropical island for your next vacation.",
+    image: "travel.jpg",
+    createdAt: "2023-08-22T12:00:00Z",
+  },
+  {
+    id: 5,
+    title: "Health and Fitness Tips",
+    details:
+      "Stay fit and healthy with these expert-recommended tips and exercises.",
+    image: "fitness.jpg",
+    createdAt: "2023-08-21T09:20:00Z",
+  },
+  {
+    id: 6,
+    title: "Culinary Adventures",
+    details:
+      "Discover unique recipes from around the world and try your hand at international cuisine.",
+    image: "food.jpg",
+    createdAt: "2023-08-20T18:10:00Z",
+  },
+  {
+    id: 7,
+    title: "Movie Night Recommendations",
+    details:
+      "Check out the top-rated movies to enjoy with your friends and family this weekend.",
+    image: "movies.jpg",
+    createdAt: "2023-08-19T20:00:00Z",
+  },
+  {
+    id: 8,
+    title: "Environmental Awareness",
+    details:
+      "Learn about conservation efforts and how you can make a positive impact on the environment.",
+    image: "environment.jpg",
+    createdAt: "2023-08-18T15:50:00Z",
+  },
+  {
+    id: 9,
+    title: "Financial Market Update",
+    details:
+      "Stay informed about the latest developments in the financial world and investment strategies.",
+    image: "finance.jpg",
+    createdAt: "2023-08-17T11:30:00Z",
+  },
+  {
+    id: 10,
+    title: "Art and Culture Highlights",
+    details:
+      "Explore the vibrant world of art and culture with featured exhibitions and performances.",
+    image: "art.jpg",
+    createdAt: "2023-08-16T17:00:00Z",
+  },
+];
