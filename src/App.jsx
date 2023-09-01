ab4c7f0daaa5421bc8bab577a3f916e99ea3e78b
Fred Vuni <fredvuni809@gmail.com>
2023-09-01 13:08:22 +0300

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <div>
       <Articles/>
+      <Articles/>
+      <Articles/>
     </div>
   )
 }
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Articles = () => {
   return (
-    <div>Articles</div>
-  )
-}
+    <div class="article-container">
+      <div class="article">
+        <img src="soccer.jpg" alt="Soccer Image" class="article-image" />
+        <div class="article-content">
+          <h2 class="article-title">Exciting Soccer Match</h2>
+          <p class="article-details">
+            In a thrilling match, the home team secured victory with a
+            last-minute goal.
+          </p>
+          <p class="article-date">Published: 2023-08-25</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Articles
+export default Articles;
