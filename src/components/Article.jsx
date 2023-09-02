88fd88bd6d780781c6ddc07c1a91a045620a6636
Fred Vuni <fredvuni809@gmail.com>
2023-09-02 14:30:23 +0300

    switch from row data to api - strapi

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,15 +5,15 @@ const Article = ({ article }) => {
     <a href="">
       <div className="article">
         <img
-          src={article.image}
-          alt={article.title}
+          src={article.attributes.image}
+          alt={article.attributes.title}
           className="article-image"
         />
         <div className="article-content">
-          <h2 className="article-title">{article.title}</h2>
-          <p className="article-details">{article.details}</p>
+          <h2 className="article-title">{article.attributes.title}</h2>
+          <p className="article-details">{article.attributes.content}</p>
           <small className="article-date">
-            Published: {new Date(article.createdAt).toDateString()}
+            Published: {new Date(article.attributes.date).toDateString()}
           </small>
         </div>
       </div>
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { stories } from "../data";
 import Article from "./Article";
 
 const Articles = () => {
+  const [articles, setArticles] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await fetch("http://localhost:1337/api/articles");
+      const data = await response.json();
+      setArticles(data.data);
+      console.log(articles);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getPosts();
+  }, [articles]);
+
   return (
     <div className="article-container">
-      {stories.map((article) => (
+      {articles.map((article) => (
         <Article key={article.id} article={article} />
       ))}
     </div>
